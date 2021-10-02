e9657b0b41d0003a9ad0654469ec066b76bcb989
Mirlan-code <almirka02261999@gmail.com>
2021-10-02 22:26:15 +0300

    123

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main (int, char **) {
-    std::cout << "Version: " << version() << std::endl;
+    std::cout << "build " << version() << std::endl;
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
